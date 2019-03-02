a13c12fcddb0ddfd5caca6406a1436ef0fb59176
Ravi Vyas <ravya@microsoft.com>
2019-03-02 12:31:13 -0800

    added multiple incrementing buttons

---
+++ multipleButtonCounter.jsx
@@ -0,0 +1,34 @@
+function Button(props) {
+	
+    const handleClick = () => props.onClickFunction(props.increment);
+      return (
+        <button onClick = {handleClick}>
+          +{props.increment}
+      </button>
+    );
+  }
+  
+  function Display(props){
+      return (
+        <div>{props.message}</div>
+    );
+  }
+  
+  function App(){
+      const[counter, setCounter] = useState(42);
+    const incrementCounter = (incrementBy) => setCounter(counter + incrementBy);
+      return (
+        <div>
+            <Button onClickFunction={incrementCounter} increment = {1}/>
+            <Button onClickFunction={incrementCounter} increment = {5}/>
+            <Button onClickFunction={incrementCounter} increment = {10}/>
+            <Button onClickFunction={incrementCounter} increment = {100}/>
+            <Display message={counter}/>
+      </div>
+    );
+  }
+  
+  ReactDOM.render(
+    <App />,
+    document.getElementById('mountNode'),
+  );
