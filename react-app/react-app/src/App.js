a827ecbc4d99940ecb5d7efa4d4a7184accabf6c
Ravi Vyas <rvyas@Ravis-MacBook-Pro.local>
2019-09-07 18:18:42 -0700

    cosmetic updates

--- react-app/react-app/src/App.js
+++ react-app/react-app/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>I'm an App</h1>
+      <p>this is working</p>
     </div>
+    //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi!!! I\'m from a react create element method'))
   );
 }
 
