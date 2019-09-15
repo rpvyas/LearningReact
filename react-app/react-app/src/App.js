83ff897bbde112523b3eb3ee0bb220ca867536be
Ravi Vyas <rvyas@Ravis-MacBook-Pro.local>
2019-09-15 16:47:59 -0700

    added Person component

--- react-app/react-app/src/App.js
+++ react-app/react-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>I'm an App</h1>
       <p>this is working</p>
+      <Person/>
     </div>
     //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi!!! I\'m from a react create element method'))
   );
