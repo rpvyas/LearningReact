430e74606ab0a391f4372ad2a1b2dd2b95aa276d
Ravi Vyas <rvyas@Ravis-MacBook-Pro.local>
2019-09-16 08:35:10 -0700

    added props

--- react-app/react-app/src/App.js
+++ react-app/react-app/src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <h1>I'm an App</h1>
       <p>this is working</p>
-      <Person/>
+      <Person name="Ravi" age="28"/>
+      <Person name="Aditya" age="31"/>
+      <Person name="Melissa" age="24"/>
     </div>
     //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi!!! I\'m from a react create element method'))
   );
