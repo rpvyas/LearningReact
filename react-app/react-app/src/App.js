6e836d6fe0a30aae47aca5c8b1160b74bce76ad9
Ravi Vyas <rvyas@Ravis-MacBook-Pro.local>
2019-09-17 19:06:46 -0700

    added state property

--- react-app/react-app/src/App.js
+++ react-app/react-app/src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>I'm an App</h1>
-      <p>this is working</p>
-      <Person name="Ravi" age="28"/>
-      <Person name="Aditya" age="31"/>
-      <Person name="Melissa" age="24"/>
-    </div>
-    //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi!!! I\'m from a react create element method'))
-  );
+class App extends Component {
+  state = {
+    persons : [
+      {name: "Ravi", age: 28},
+      {name: "Aditya", age: 31},
+      {name: "Melissa", age: 24}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>I'm an App</h1>
+        <p>this is working</p>
+        <button> Switch Names </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>MyHobbies : News</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+      //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi!!! I\'m from a react create element method'))
+    );
+  }
 }
 
 export default App;
