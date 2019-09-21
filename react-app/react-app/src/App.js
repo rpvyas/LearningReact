c592cda3a91e846d5975aa31a37e467eed2f64c8
Ravi Vyas <rvyas@Ravis-MacBook-Pro.local>
2019-09-21 15:31:39 -0700

    added conditional render

--- react-app/react-app/src/App.js
+++ react-app/react-app/src/App.js
@@ -55,24 +55,30 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showNames) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age} 
+            click={this.switchNameHandler.bind(this,"rpvyas")}
+            change={this.nameChangeHandler} >MyHobbies : News
+          </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        </div> 
+      );
+    }
+
+
     return (
       <div className="App">
         <h1>I'm an App</h1>
         <p>this is working</p>
         <button style={style} onClick = {this.togglePersonHandler}> Toggle Names </button>
-        {this.state.showNames === true ? 
-          <div>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age} 
-                click={this.switchNameHandler.bind(this,"rpvyas")}
-                change={this.nameChangeHandler} >MyHobbies : News
-              </Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-          </div> : null
-
-        }
+        {persons}
         
       </div>
       //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi!!! I\'m from a react create element method'))
