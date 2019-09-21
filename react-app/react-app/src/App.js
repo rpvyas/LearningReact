3e57ef69a24d91b6fcaceacb4468c0c285865f6e
Ravi Vyas <rvyas@Ravis-MacBook-Pro.local>
2019-09-21 16:23:14 -0700

    adding lists

--- react-app/react-app/src/App.js
+++ react-app/react-app/src/App.js
@@ -60,14 +60,17 @@ class App extends Component {
     if(this.state.showNames) {
       persons = (
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          {this.state.persons.map(person => {
+            return <Person name = {person.name} age = {person.age}/>
+          })}
+          {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
           <Person 
             name={this.state.persons[1].name} 
             age={this.state.persons[1].age} 
             click={this.switchNameHandler.bind(this,"rpvyas")}
             change={this.nameChangeHandler} >MyHobbies : News
           </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/> */}
         </div> 
       );
     }
